b5c9f3f806f9f1ace8b1890aeae7cbb15611fd05
rpariona81 <ronald@RPARIONA-PCDELL.localdomain>
2023-04-04 00:17:17 -0500

    tabla iconos tabler

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -30,6 +30,37 @@
                         </a>
                     </li>
 
+                    <li class="nav-item @if(request()->routeIs('menus.index')) active @endif">
+                        <a class="nav-link" href="{{ route('menus.index') }}" >
+                            <svg xmlns="{{ url('/svg') }}" class="icon icon-tabler icon-tabler-2fa" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M7 16h-4l3.47 -4.66a2 2 0 1 0 -3.47 -1.54"></path>
+                                <path d="M10 16v-8h4"></path>
+                                <path d="M10 12l3 0"></path>
+                                <path d="M17 16v-6a2 2 0 0 1 4 0v6"></path>
+                                <path d="M17 13l4 0"></path>
+                             </svg>
+                            <span class="nav-link-title">
+                                {{ __('Otros') }}
+                            </span>
+                        </a>
+                    </li>
+
+                    <li class="nav-item @if(request()->routeIs('menus.index')) active @endif">
+                        <a class="nav-link" href="{{ route('menus.index') }}" >
+                            <svg xmlns="{{ url('/svg') }}"  class="icon icon-tabler icon-tabler-award-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M16.72 12.704a6 6 0 0 0 -8.433 -8.418m-1.755 2.24a6 6 0 0 0 7.936 7.944"></path>
+                                <path d="M12 15l3.4 5.89l1.598 -3.233l.707 .046m1.108 -2.902l-1.617 -2.8"></path>
+                                <path d="M6.802 12l-3.4 5.89l3.598 -.233l1.598 3.232l3.4 -5.889"></path>
+                                <path d="M3 3l18 18"></path>
+                            </svg>
+                            <span class="nav-link-title">
+                                {{ __('Muchos más') }}
+                            </span>
+                        </a>
+                    </li>
+
                     <li class="nav-item @if(request()->routeIs('users.index')) active @endif">
                         <a class="nav-link" href="{{ route('users.index') }}" >
                             <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/file-text -->
